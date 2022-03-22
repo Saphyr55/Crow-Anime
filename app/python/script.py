c36da770793963ae7aaa1e1576e7b6aaa432d8b0
Saphyr55 <saphyr0550@gmail.com>
2022-03-22 20:34:50 +0100

    add : script python qui recuperer l'url de l'image de profil d'un anime/manga depuis MAL

---
+++ app/python/script.py
@@ -0,0 +1,34 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+name_anime = "My Teen Romantic Comedy SNAFU"
+id_anime = 0
+
+# recupere la page de l'anime ou manga
+driver = webdriver.Chrome(executable_path="app/python/chromedriver")
+driver.get("https://myanimelist.net/search/all?q="+name_anime)
+driver.implicitly_wait(3)
+
+all_animes = driver.find_elements(By.CLASS_NAME ,"hoverinfo_trigger")
+url_anime = all_animes[0].get_attribute("href")
+
+# recupere la url de l'image de l'anime de profil et l'enregistre dans fichier json
+url_image_anime = driver.find_elements(By.CLASS_NAME, "lazyloaded")[0].get_attribute("src")
+json_url_image_anime = {
+    "url" : url_image_anime
+}
+with open(f"assets/img/work/54x74/{id_anime}.json", "w") as file_json_url_image_anime:
+    json.dump(json_url_image_anime, file_json_url_image_anime, indent=2)
+
+driver.implicitly_wait(3)
+driver.get(url_anime)
+url_image_anime = driver.find_elements(By.CLASS_NAME, "lazyloaded")[0].get_attribute("src")
+json_url_image_anime = {
+    "url" : url_image_anime
+}
+with open(f"assets/img/work/225x316/{id_anime}.json", "w") as file_json_url_image_anime:
+    json.dump(json_url_image_anime, file_json_url_image_anime, indent=2)
+
+driver.implicitly_wait(3)
+driver.quit()
