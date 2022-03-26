1b1afa09a79a62bf9665094d029529d85dfa63f7
Saphyr55 <saphyr0550@gmail.com>
2022-03-26 17:32:21 +0100

    ajout test

--- src/Frontend/components/profilmanga.php
+++ src/Frontend/components/profilmanga.php
@@ -1,23 +1,23 @@
 <section>
     <div class="viewed-anime">
-            <p class="p-anime">
-                <a href="">
-                    Manga lus
-                </a>
-            </p>
-            <ul class="viewed-anime-img">
-                <?php 
-                for ($i=0; $i < 50; $i++) { 
-                    echo '
-                                    <li class="anime">
+        <p class="p-anime">
+            <a href="">
+                Manga lus
+            </a>
+        </p>
+        <ul class="viewed-anime-img">
+            <?php
+            for ($i = 0; $i < 50; $i++) {
+                echo '
+                <li class="anime">
                     <a href="">
                         <img class="anime-img" src="not_found.png" alt="" srcset="">
                         <p class="name-anime">Name manga</p>
                     </a>            
                 </li>
                     ';
-                }
-                ?>
-            </ul>
+            }
+            ?>
+        </ul>
     </div>
 </section>
