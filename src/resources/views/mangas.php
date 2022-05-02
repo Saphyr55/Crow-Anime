895077afa321c2bb82ddc64a596034593ff929c1
Saphyr55 <saphyr0550@gmail.com>
2022-05-02 21:04:54 +0200

    add href on html

--- src/resources/views/mangas.php
+++ src/resources/views/mangas.php
@@ -11,7 +11,7 @@
     <div class="list-container">
         <div class="list-items">
             <?php for ($i = 0; $i < 20; $i++) : ?>
-                <a href="" class="list-item">
+                <a href="<?= "http://$_SERVER[HTTP_HOST]/manga/".$mangas[$i]->getIdWork() ?>" class="list-item">
                     <?php if ($i <= (count($mangas) - 1)) : ?>
                         <img class="list-item-filter"
                              src="<?= "http://$_SERVER[HTTP_HOST]/assets/img/manga/" . $mangas[$i]->getIdWork() . '.jpg' ?>">
