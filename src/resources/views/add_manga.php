b91949fb8a85c67bce4296b3bfe611c11f858822
QoaTu <lucaaslc03@gmail..com>
2022-05-06 23:14:43 +0200

    Redisign Add Manga + Fix css add anime

--- src/resources/views/add_manga.php
+++ src/resources/views/add_manga.php
@@ -4,26 +4,26 @@
     </div>
     <div class="form">
         <form action="" method="POST" enctype="multipart/form-data">
-            <input type="text" name="title_en" placeholder="<?= $name_english ?>" value="<?= $title_en ?>"><br>
-            <input type="text" name="title_ja" placeholder="<?= $name_japanese ?>" value="<?= $title_ja ?>"><br>
+            <input class="input_area" type="text" name="title_en" placeholder="<?= $name_english ?>" value="<?= $title_en ?>"><br>
+            <input class="input_area" type="text" name="title_ja" placeholder="<?= $name_japanese ?>" value="<?= $title_ja ?>"><br>
             <div class="year-season">
-                <label><?= $year ?></label>
+                <label id="date_title"><?= $year ?></label>
                 <input name="date" type="date" value="<?= $date ?>"/>
             </div>
             <br>
             <div>
-                <input class="input-pa" type="text" name="publishing_house" id="" placeholder="<?= $_publishing_house ?>"
+                <input id="input_area" type="text" name="publishing_house" id="" placeholder="<?= $_publishing_house ?>"
                        value="<?= $publishing_house ?>">
-                <input class="input-pa" type="text" name="authors" id="" placeholder="<?= $_authors ?>" value="<?= $authors ?>">
+                <input id="input_area" type="text" name="authors" id="" placeholder="<?= $_authors ?>" value="<?= $authors ?>">
             </div>
             <br>
             <div>
-                <textarea class="anime-synopsis" name="manga_synopsis" placeholder="Synopsis" required><?= "$manga_synopsis" ?></textarea>
+                <textarea class="input_area" id="manga-synopsis" placeholder="Synopsis" required><?= "$manga_synopsis" ?></textarea>
             </div>
             <br>
             <div class="choose-picture">
                 <label><?= $choose_picture ?></label><br>
-                <input type="file" id="anime-picture" name="manga_picture" accept="image/png, image/jpeg">
+                <input type="file" id="picture_select" name="manga_picture" accept="image/png, image/jpeg">
             </div>
             <div>
                 <label for="finish"><?= $is_finish ?></label>
@@ -35,9 +35,11 @@
             </div>
             <br>
             <div>
-                <button class="preview-anime" type="submit" name="preview"><?= $preview ?></button>
-                <br>
-                <button class="submit-button" type="submit" name="submit"><?= $register ?></button>
+                <div class="buttons">
+                    <button class="preview-anime" type="submit" name="preview"><?= $preview ?></button>
+                    <br>
+                    <button class="submit-button" type="submit" name="submit"><?= $register ?></button>
+                </div>
             </div>
         </form>
     </div>
